ccc93614a1188c5689bd49aa80c00d98b43d9a8c
dimalebedeff <taks82ru@gmail.com>
2026-08-30 01:53:38 +0300

    Bump version to 0.10.1

--- coapp/src/host.ts
+++ coapp/src/host.ts
@@ -30,7 +30,7 @@ import type {
 } from '../../shared/protocol';
 
 // Держи в синхроне с extension/manifest.json и package.json
-const VERSION = '0.10.0';
+const VERSION = '0.10.1';
 
 // __dirname указывает на coapp/dist после сборки
 const coappRoot = path.join(__dirname, '..');
